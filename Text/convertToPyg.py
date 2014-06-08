a106648f80faee50b2ef0b9fb0d085cb298d8801
Dawit Elias <dawitelias14@gmail.com>
2014-06-08 11:05:08 -0400

    update convertToPyg.py
    Fixed double tabbing.

--- Text/convertToPyg.py
+++ Text/convertToPyg.py
@@ -1,24 +1,24 @@
 from sys import exit
 
 while True:
-		userInput = raw_input("Enter a word: ")
-		
-		# make sure user enters something    
-		if len(userInput) > len("") and userInput.isalpha():
-		
-				# convert all to lowercase letters
-				validatedInput = userInput.lower()
-				pygResult = validatedInput + validatedInput[0] + "ay"
-    
-				# take everything after the first character in pygResult
-				pygResult = pygResult[1:len(pygResult)]
-				
-				print userInput + "in pyglatin is: " + pygResult + "."
-						
-				quit = raw_input("\nEnter 'quit' to exit the program or enter any key to continue: ")
-						
-				if quit == "quit":
-						exit(0)
+	userInput = raw_input("Enter a word: ")
+	
+	# make sure user enters something    
+	if len(userInput) > len("") and userInput.isalpha():
+	
+		# convert all to lowercase letters
+		validatedInput = userInput.lower()
+		pygResult = validatedInput + validatedInput[0] + "ay"
 
-		else:
-				print "Error. Please try again."
+		# take everything after the first character in pygResult
+		pygResult = pygResult[1:len(pygResult)]
+		
+		print userInput + "in pyglatin is: " + pygResult + "."
+				
+		quit = raw_input("\nEnter 'quit' to exit the program or enter any key to continue: ")
+				
+		if quit == "quit":
+				exit(0)
+	
+	else:
+		print "Error. Please try again."

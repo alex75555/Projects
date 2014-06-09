e09df0dc4582ab7eb88ac18c7f970222db3b47ed
Dawit Elias <dawitelias14@gmail.com>
2014-06-08 21:53:35 -0400

    Created reverseString.js
    Reverse the order of any string!

---
+++ Text/reverseString.js
@@ -0,0 +1,11 @@
+// Dawit Elias
+// June 8, 2014
+// dawitelias14@gmail.com
+
+// add any string when function is called to see it reversed.
+
+function reverseIt(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseIt("Hi there"));
